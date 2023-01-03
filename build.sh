e8fd976c1912a5f33dfdf9b054ea0221c4ab4165
SleepDevil <1752915704@qq.com>
2023-01-03 15:10:07 +0800

    feat: update bash script to avoid build gh-pages branch

--- build.sh
+++ build.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
+echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
+
+if [[ "$VERCEL_GIT_COMMIT_REF" == "gh-pages" ]] ; then
+  # Don't build
+    echo "× build stop beacause of github page branch"
+  exit 0;
+  else
+  # Proceed with the build
+    exit 1;
+fi
 
 if [ -z ${BASE_URL} ]; then
     echo "BASE_URL is unset, use VERCEL: '$VERCEL_URL'";
